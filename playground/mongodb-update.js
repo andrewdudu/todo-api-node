9b90b14e38dd384b8a593846c532f096c0af3874
Dudu <jun.quan.mail@gmail.com>
2018-07-23 22:31:56 +0700

    add POST /todos route and refactor mongoose

---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to database');
+  }
+  console.log('Connected to database');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b558f7e8aab373be4da89d6')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // })
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b558ad98aab373be4da89d4')
+  }, {
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // db.close();
+})
