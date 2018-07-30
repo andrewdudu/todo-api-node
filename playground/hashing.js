dbdd45281e0b76f2c2a31b8230f4835ffc81dc0d
Dudu <jun.quan.mail@gmail.com>
2018-07-30 11:32:41 +0700

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = 'abc123!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// })
+
+var hashedPass = '$2a$10$.Qf6gko6unk5LNUidUA3Iu2RzS8wQZ9g47CRodj7ZobahekWDf4ye';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+  console.log(res);
+})
+
+// var data = {
+//   id: 10
+// }
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
 
 
